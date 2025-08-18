045dacd9e7532f5682adb39e3b466c9873b748bb
Ludus <alessio.desantis28@gmail.com>
2025-08-18 16:04:11 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 Scripulous Fingore is a gaming urban legend, a character said to be hidden in many games, included in the files but never actually used.
 
-This mod finally brings him to life, something that should’ve happened a long time ago.
+This mod finally brings him to life, something that should’ve happened a long time ago; he is a hostile mob that spawns in the darkness.
 
-
-Scripulous Fingore is a hostile mob that spawns in caves below Y level 0.
+Scripulous Fingore is also very smart, infact every time you will say something in chat, he will brillantly answer: "Nuh uh".
--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ minetest.register_entity("fingore:scripulous", {
     initial_properties = {
         physical = true,
         collide_with_objects = true,
-        collisionbox = {-1, -1, -1, 1, 1, 1},
+        collisionbox = {-0.3, -0.4, -0.3, 0.3, 0.4, 0.3},
         visual = "mesh",
         mesh = "fingore.gltf",
         textures = {"fingore.png"},
@@ -14,6 +14,14 @@ minetest.register_entity("fingore:scripulous", {
 
     timer = 0,
     attack_cooldown = 0,
+
+    on_activate = function(self, staticdata, dtime_s)
+        if math.random(1, 4) == 1 then
+            self.object:set_properties({
+                mesh = "fingore_nuhuh.gltf",
+            })
+        end
+    end,
 
     on_step = function(self, dtime)
         self.timer = self.timer + dtime
@@ -45,10 +53,12 @@ minetest.register_entity("fingore:scripulous", {
                 local dist = vector.length(dir)
                 if dist > 0 then
                     dir = vector.normalize(dir)
-                    self.object:set_velocity(vector.multiply(dir, 2))
+                    local speed = 3.5 
+                    self.object:set_velocity(vector.multiply(dir, speed))
                     self.object:set_yaw(math.atan2(dir.x, dir.z))
                 end
                 if dist <= 1.5 and self.attack_cooldown <= 0 then
+                    self.object:set_velocity(vector.multiply(dir, 6))
                     nearest_player:punch(self.object, 1.0, {
                         full_punch_interval = 1.0,
                         damage_groups = {fleshy = 15},
@@ -61,23 +71,25 @@ minetest.register_entity("fingore:scripulous", {
             self.object:set_velocity({x = 0, y = velocity.y, z = 0})
         end
     end,
-on_punch = function(self, hitter)
+
+    on_punch = function(self, hitter)
         if hitter and hitter:is_player() then
             minetest.chat_send_all("Scripulous Fingore fades into the darkness...")
 
             local pos = self.object:get_pos()
             if pos then
-                if math.random(25, 100) == 25 then --25%
-                minetest.add_item(pos, "fingore:fingore_egg")
-                if math.random(1, 100) == 1 then --1%
-                    minetest.add_item(pos, "fingore:fingore_hand")
+                if math.random(1, 4) == 1 then -- 25%
+                    minetest.add_item(pos, "fingore:fingore_egg")
+                    if math.random(1, 100) == 1 then -- 1%
+                        minetest.add_item(pos, "fingore:fingore_hand")
+                    end
                 end
             end
         end
-    end
         self.object:remove()
     end,
 })
+
 
 minetest.register_chatcommand("spawn_fingore", {
     description = "Spawn Scripulous Fingore",
@@ -106,29 +118,25 @@ minetest.register_chatcommand("spawn_fingore", {
 local function try_spawn_fingore()
     for _, player in ipairs(minetest.get_connected_players()) do
         local pos = player:get_pos()
-        if pos and pos.y < 0 then
-            local under_pos = vector.offset(pos, 0, -1, 0)
-            local node = minetest.get_node_or_nil(under_pos)
+        if pos then
+            local light = minetest.get_node_light(pos, 0.5) or 15
+            if light <= 10 and math.random(1, 500) == 1 then
+                local spawn_offset = {
+                    x = math.random(-3, 3),
+                    y = 1,
+                    z = math.random(-3, 3),
+                }
+                local spawn_pos = vector.add(pos, spawn_offset)
+                local spawn_node = minetest.get_node_or_nil(spawn_pos)
 
-            if node and node.name ~= "ignore" then
-                local light = minetest.get_node_light(pos, 0.5) or 15
-                if light <= 7 and math.random(1, 7000) == 1 then
-                    local spawn_offset = {
-                        x = math.random(-3, 3),
-                        y = 1,
-                        z = math.random(-3, 3),
-                    }
-                    local spawn_pos = vector.add(pos, spawn_offset)
-                    local spawn_node = minetest.get_node_or_nil(spawn_pos)
-                    if spawn_node and spawn_node.name == "air" then
-                        minetest.add_entity(spawn_pos, "fingore:scripulous")
-                        minetest.chat_send_all("You feel an evil presence lurking nearby...")
-                    end
+                if spawn_node and spawn_node.name == "air" then
+                    minetest.add_entity(spawn_pos, "fingore:scripulous")
                 end
             end
         end
     end
 end
+
 
 local global_timer = 0
 minetest.register_globalstep(function(dtime)
@@ -171,3 +179,13 @@ minetest.register_tool("fingore:fingore_hand", {
     },
     sound = {breaks = "default_tool_breaks"},
 })
+
+-- nuh uh 
+minetest.register_on_chat_message(function(name, message)
+    minetest.after(0.1, function()
+        minetest.chat_send_all("<Scripulous Fingore> Nuh uh")
+    end)
+    return false 
+end)
+
+
